098712abe7743f91f8edacc1a9a35758c8551122
Damien Heulin <damienheulin87@gmail.com>
2022-05-03 10:45:46 +0200

    Added GraphQL and Prisma

---
+++ pages/api/index.ts
@@ -0,0 +1,97 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { PrismaClient } from "@prisma/client";
+
+import { join } from "path";
+import { readFileSync } from "fs";
+import { createServer } from "@graphql-yoga/node"
+
+import currencyFormatter from "currency-formatter";
+
+import prisma from "../../lib/prisma";
+import { Resolvers } from "../../types";
+
+export type GraphQLContext = {
+    prisma: PrismaClient;
+};
+
+export async function createContext(): Promise<GraphQLContext> {
+    return {
+        prisma,
+    };
+}
+
+const currencyCode = "EUR";
+
+const typeDefs = readFileSync(join(process.cwd(), "schema.graphql"), {
+    encoding: "utf-8",
+});
+
+const resolvers: Resolvers = {
+    Query: {
+        cart: async (_, { id }, { prisma }) => {
+            let cart = await prisma.cart.findUnique({
+                where: { id },
+            });
+
+            if (!cart) {
+                cart = await prisma.cart.create({
+                    data: { id },
+                });
+            }
+
+            return cart;
+        },
+    },
+    Cart: {
+        items: async ({ id }, _, { prisma }) => {
+            const items = await prisma.cart
+                .findUnique({
+                    where: { id },
+                })
+                .items();
+
+            return items;
+        },
+        totalItems: async ({ id }, _, { prisma }) => {
+            const items = await prisma.cart
+                .findUnique({
+                    where: { id },
+                })
+                .items();
+
+            return items.reduce((total, item) => total + item.quantity || 1, 0);
+        },
+        subTotal: async ({ id }, _, { prisma }) => {
+            const items = await prisma.cart
+                .findUnique({
+                    where: { id },
+                })
+                .items();
+
+            const amount =
+                items.reduce((acc, item) => acc + item.price * item.quantity, 0) ?? 0;
+
+            return {
+                amount,
+                formatted: currencyFormatter.format(amount / 100, {
+                    code: currencyCode,
+                }),
+            };
+        },
+    }
+};
+
+const server = createServer<{
+    req: NextApiRequest;
+    res: NextApiResponse;
+}>({
+    cors: false,
+    endpoint: "/api",
+    schema: {
+        typeDefs,
+        resolvers,
+    },
+    context: createContext(),
+})
+
+export default server
