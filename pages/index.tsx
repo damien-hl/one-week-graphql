8c5abf3a4665fcb9d8c4a68a683a16fdc8106834
Damien Heulin <damienheulin87@gmail.com>
2022-05-15 11:29:03 +0200

    Product grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
 
 import { Header } from "../components/Header";
+import { ProductList } from '../components/ProductList';
+import { Product, products } from '../lib/products';
 
-const Home: NextPage = () => {
-  return <Header />;
+const Home: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
+  products
+}) => {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section>
+          <ProductList products={products} />
+        </section>
+      </main>
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps<{
+  products: Product[];
+}> = async () => ({
+  props: {
+    products: products.slice(0, 6),
+  },
+});
 
 export default Home
