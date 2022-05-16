5b0526ad63c1ae2a9e827e5e25ead65e6bd076d2
Damien Heulin <damienheulin87@gmail.com>
2022-05-16 13:55:47 +0200

    Add to cart button

--- components/ProductDetail.tsx
+++ components/ProductDetail.tsx
@@ -1,8 +1,17 @@
 import type { Product } from "../lib/products";
 
+import { getCookie } from "cookies-next";
+
+import { GetCartDocument, useAddToCartMutation } from "../types";
 import { ProductItem } from "./ProductItem";
 
 export function ProductDetail({ product }: { product: Product | null }) {
+    const cartId = String(getCookie("cartId"));
+
+    const [addToCart, { loading }] = useAddToCartMutation({
+        refetchQueries: [GetCartDocument],
+    });
+
     if (!product) {
         return null;
     }
@@ -15,6 +24,32 @@ export function ProductDetail({ product }: { product: Product | null }) {
             <div className="p-8 space-y-4">
                 <div dangerouslySetInnerHTML={{ __html: product.body }} />
             </div>
+            <form
+                className="p-8 space-y-4"
+                onSubmit={(e) => {
+                    e.preventDefault();
+                    addToCart({
+                        variables: {
+                            input: {
+                                cartId,
+                                id: product.id,
+                                name: product.title,
+                                description: product.body,
+                                price: product.price,
+                                image: product.src,
+                            },
+                        },
+                    });
+                }}
+            >
+                <div dangerouslySetInnerHTML={{ __html: product.body }} />
+                <button
+                    className="px-6 py-4 bg-black rounded w-full text-white hover:bg-white hover:text-black border border-black uppercase"
+                    type="submit"
+                >
+                    {loading ? "Adding to cart..." : "Add to cart"}
+                </button>
+            </form>
         </main>
     );
 }
