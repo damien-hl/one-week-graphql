8c4d95c99da0d784236d62215f53ca0d66e1affe
Damien Heulin <damienheulin87@gmail.com>
2022-05-16 13:59:20 +0200

    Cart summary page

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,6 +1,7 @@
 import { GetServerSideProps, NextPage } from "next";
 
 import { Header } from "../components/Header";
+import { CartDetail } from "../components/CartDetail";
 
 import { getCartId } from "../lib/cart.client";
 import { useGetCartQuery } from "../types";
@@ -14,11 +15,7 @@ const Cart: NextPage<IProps> = ({ cartId }) => {
             <main className="p-8 min-h-screen">
                 <div className="mx-auto max-w-xl space-y-8">
                     <h1 className="text-4xl">Cart</h1>
-                    <div>Items: {data?.cart?.totalItems}</div>
-                    <div className="border-t pt-4 flex justify-between">
-                        <div>Subtotal</div>
-                        <div>{data?.cart?.subTotal.formatted}</div>
-                    </div>
+                    <CartDetail cart={data?.cart} />
                 </div>
             </main>
         </div>
